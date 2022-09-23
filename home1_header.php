753da99a080a252c2c0b537505981fbc77067525
Nguyen Hoang <vinhthihoang2@gmail.com>
2022-09-23 17:34:14 +0700

    5:33 23/9/2022

---
+++ home1_header.php
@@ -0,0 +1,23 @@
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+</head>
+<body>
+
+    <header class="header container-fluid">
+        <div class="row header_row-1">
+            <div class="header_banner-1 text-capitalize">easier and more comfortable while on vacation with us</div>
+        </div>
+    </header>
+
+<script src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
