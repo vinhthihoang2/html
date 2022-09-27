f055c061eb9edc78ba28190a3876e8025d71cfd5
Nguyen Hoang <vinhthihoang2@gmail.com>
2022-09-27 17:36:30 +0700

    5:36 27/09/2022

--- home1.php
+++ home1.php
@@ -11,9 +11,22 @@
 <main class="main_content container-fluid">
     <div class="container main_content_1">
         <div class="d-flex flex-wrap">
-            <div class="col-6 main_content_1_context">
-                <p>Fun <p>Vacation</p> Pefect Location</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu condimentum mattis vitae sem convallis sit cras pellentesque facilisis. Morbi commodo.</p>
+            <div class="col-6 main_content_1_context d-flex flex-wrap align-items-center">
+                <div class="main_content_1_title">
+                    Fun <span style="color: #EA494E;">Vacation</span> Pefect Location
+                </div>
+
+                <div class="main_content_1_context">
+                    <p class="col-9 pe-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu condimentum mattis vitae sem convallis sit cras pellentesque facilisis. Morbi commodo.</p>
+                </div>
+
+                <div class="d-flex">
+                    <button class="btn_explore py-3 px-4 me-5">Explore Now<img class="ps-2" src="img/Home1/arrow_1.png"></button>
+                    <a href="#" class="demo_travel d-flex align-items-start pb-0 align-items-center">
+                        <img class="me-2" src="img/Home1/Vector.png">
+                        <div>Demo Travel</div>
+                    </a>
+                </div>
             </div>
 
             <div class="col-6 main_content_1_bg mt-5"><div class="main_content_1_img"></div></div>
@@ -43,6 +56,41 @@
             </div>
         </div>
     </div>
+
+    <div class="container-fluid tpd_bg">
+        <div class="container main_content_tpd py-5 my-5">
+            <div class="d-flex">
+                <div class="col tpd_title">Top Popular  Destination</div>
+                <button class="col-2 tpd_explore text-end align-middle">Explore Now<img class="ps-2" src="img/Home1/arrow_2.png"></button>
+            </div>
+
+            <div class="gallery pt-5">
+                <div class="row">
+                    <div class="col-4">
+                        <img class="img-thumbnail" src="img/Home1/tpd_img_1.png">
+                    </div>
+
+                    <div class="col-8">
+                        <div class="row">
+                            <div class="col-6"><img class="img-thumbnail" src="img/Home1/tpd_img_2.png"></div>
+                            <div class="col-6"><img class="img-thumbnail" src="img/Home1/tpd_img_3.png"></div>
+                        </div>
+
+                        <div class="col-12">
+                            <img class="img-thumbnail" src="img/Home1/tpd_img_4.png">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="container">
+
+    </div>
+
 </main>
 </body>
 </html>
